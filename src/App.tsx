17b52737a574b1bb59bc7d317fd05043ab5d74ab
Eddy <oslau100@gmail.com>
2026-06-14 21:47:05 +0200

    Initialize App component with routing and providers

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CookieConsent } from "./components/CookieConsent";
+import Index from "./pages/Index";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Survey from "./pages/Survey";
+import Privacy from "./pages/Privacy";
+import Imprint from "./pages/Imprint";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/ueber-uns" element={<About />} />
+          <Route path="/start" element={<Survey />} />
+          <Route path="/datenschutz" element={<Privacy />} />
+          <Route path="/impressum" element={<Imprint />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <CookieConsent />
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
