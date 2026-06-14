f4d5f06384012e66779d46db414ff9f2fa673fe0
Eddy <oslau100@gmail.com>
2026-06-14 18:44:04 +0200

    Delete src directory

--- src/components/ui/popover.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react"; import * as PopoverPrimitive from "@radix-ui/react-popover"; import { cn } from "@/lib/utils";
-const Popover=PopoverPrimitive.Root; const PopoverTrigger=PopoverPrimitive.Trigger;
-const PopoverContent=React.forwardRef<any,any>(({className,align="center",sideOffset=4,...props},ref)=><PopoverPrimitive.Portal><PopoverPrimitive.Content ref={ref} align={align} sideOffset={sideOffset} className={cn("z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md",className)} {...props}/></PopoverPrimitive.Portal>);
-export { Popover, PopoverTrigger, PopoverContent };
--- src/pages/Uebermittelt.tsx
+++
@@ -1,8 +0,0 @@
-import { SimpleHeader } from "@/components/SimpleHeader";
-import { SimpleFooter } from "@/components/SimpleFooter";
-
-const Uebermittelt = () => (
-  <div className="min-h-screen flex flex-col bg-background text-foreground font-sans selection:bg-primary/20 selection:text-primary pt-24 md:pt-32"><SimpleHeader /><main className="flex-grow flex items-center justify-center p-4">{/* Freier Bereich für /uebermittelt */}</main><SimpleFooter /></div>
-);
-
-export default Uebermittelt;
